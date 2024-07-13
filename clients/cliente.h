4b810b83e4146aedba5d9061477780b2987453ea
JonasCGN <jonasbo66@gmail.com>
2024-07-12 22:12:17 -0300

    Iniciando repositorio Trabalho ED

---
+++ clients/cliente.h
@@ -0,0 +1,19 @@
+#pragma once
+
+typedef struct{
+    char nome[50];
+    char cpf[15];
+    char telefone[15];
+    char email[50];
+    char endereco[100];
+}Cliente;
+
+typedef struct listaCLiente{
+    Cliente cliente;
+    struct listaCliente *prox;
+}ListaCLiente;
+
+ListaCLiente criaListaCliente();
+Cliente cadastro();
+void cadastroCliente(ListaCLiente **cliente);
+void exibirClientes(ListaCLiente *cliente);
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "routes/routes.h"
+#include "clients/cliente.h"
+
+int main(){
+
+	return 0;
+}
